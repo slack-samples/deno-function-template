e35d2b9b177458b8cdd39f5ab5d24020c3dcdb96
Ethan Zimbelman <ethan.zimbelman@me.com>
2024-02-20 14:33:57 -0800

    test: assert function error is undefined

--- functions/sample_function_test.ts
+++ functions/sample_function_test.ts
@@ -19,7 +19,8 @@ mf.mock("POST@/api/chat.postMessage", () => {
 
 Deno.test("Sample function test", async () => {
   const inputs = { message: "Hello, World!", user: "U01234567" };
-  const { outputs } = await SampleFunction(createContext({ inputs }));
+  const { outputs, error } = await SampleFunction(createContext({ inputs }));
+  assertEquals(error, undefined);
   assertEquals(outputs?.message, "Hello, World!");
   assertEquals(outputs?.user, "U01234567");
 });
