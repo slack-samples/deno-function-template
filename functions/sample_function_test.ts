58a93cd1f567340149667ed317094dfffcfcc3ca
William Bergamin <wbergamin@salesforce.com>
2025-05-29 10:44:31 -0400

    chore: update to latests Deno and CLI standards (#29)

--- functions/sample_function_test.ts
+++ functions/sample_function_test.ts
@@ -1,23 +1,28 @@
 import { SlackFunctionTester } from "deno-slack-sdk/mod.ts";
-import { assertEquals } from "https://deno.land/std@0.153.0/testing/asserts.ts";
+import { assertEquals } from "@std/assert";
+import { stub } from "@std/testing/mock";
 import SampleFunction from "./sample_function.ts";
-import * as mf from "https://deno.land/x/mock_fetch@0.3.0/mod.ts";
 
 const { createContext } = SlackFunctionTester("sample_function");
 
-// Replaces globalThis.fetch with the mocked copy
-mf.install();
+Deno.test("Sample function test", async () => {
+  // Replaces globalThis.fetch with the mocked copy
+  using _fetchStub = stub(
+    globalThis,
+    "fetch",
+    (url: string | URL | Request, options?: RequestInit) => {
+      const request = url instanceof Request ? url : new Request(url, options);
 
-mf.mock("POST@/api/chat.postMessage", () => {
-  return new Response(
-    `{"ok": true}`,
-    {
-      status: 200,
+      assertEquals(request.method, "POST");
+      assertEquals(request.url, "https://slack.com/api/chat.postMessage");
+      return Promise.resolve(
+        new Response(JSON.stringify({ ok: true }), {
+          status: 200,
+        }),
+      );
     },
   );
-});
 
-Deno.test("Sample function test", async () => {
   const inputs = { message: "Hello, World!", user: "U01234567" };
   const { outputs, error } = await SampleFunction(createContext({ inputs }));
   assertEquals(error, undefined);
